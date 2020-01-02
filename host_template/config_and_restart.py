cb3e55f5f0254c77720fa57f9b9555c8caa02a87
tqre <tkuure@gmail.com>
2020-01-02 18:23:13 +0200

    Templatization

--- host_template/config_and_restart.py
+++ host_template/config_and_restart.py
@@ -1,23 +1,28 @@
 # UpCloud API-Calls
-# Configure Arch Linux cloud settings after arch_host_install script
+# Templatize created Arch Linux VM and deploy one while getting
+# rid of the initial VM
+#
+# Before templatization, change the password for sudo user!
+
 import http.client
+import json
+import time
 from base64 import b64encode
 from getpass import getpass
+
+# Server's uuid is needed to modify it, server has to be shut down
+uuid = ""
 
 user = input("Username:")
 pswd = getpass("Password:")
 login = b64encode((user + ":" + pswd).encode())
 conn = http.client.HTTPSConnection("api.upcloud.com")
 headers = {
-"Authorization"	: "Basic " + login.decode(),
-"Content-Type"	: "application/json"
+	"Authorization"	: "Basic " + login.decode(),
+	"Content-Type"	: "application/json"
 }
 
-# Server's uuid is needed to modify it
-uuid = ""
-
 # Change boot_options to boot from primary HD
-# Display adapter is Cirrus?
 body = open("bootfromHD.json", "r")
 conn.request("PUT", "/1.3/server/" + uuid, body, headers)
 print(conn.getresponse().read().decode(encoding="UTF-8"))
@@ -29,8 +34,39 @@ conn.request("POST", "/1.3/server/" + uuid + "/storage/detach", body, headers)
 print(conn.getresponse().read().decode(encoding="UTF-8"))
 body.close()
 
-# Start the server
-conn.request("POST", "/1.3/server/" + uuid + "/start", None, headers)
+# Get the attached storage UUID from the server
+conn.request("GET", "/1.3/server/" + uuid, None, headers)
+response = json.loads(conn.getresponse().read().decode(encoding="UTF-8"))
+storageuuid = response["server"]["storage_devices"]["storage_device"][0]["storage"]
+
+# Templatize the storage
+body = open("template_config.json", "r")
+conn.request("POST", "/1.3/storage/" + storageuuid + "/templatize", body, headers)
+body.close()
+
+# This returns the template uuid
+response = json.loads(conn.getresponse().read().decode(encoding="UTF-8"))
+templateuuid = response["storage"]["uuid"]
+
+# Editing clones.json to reflect the new template uuid
+with open("clones.json", "r") as file:
+	data = json.load(file)
+
+with open("clones.json", "w") as file:
+	data["server"]["storage_devices"]["storage_device"][0]["storage"] = templateuuid
+	json.dump(data, file, indent=2)
+
+# We can ditch the original server  now
+conn.request("DELETE", "/1.3/server/" + uuid + "/?storages=1", None, headers)
 print(conn.getresponse().read().decode(encoding="UTF-8"))
+
+# Then start making servers out of the template
+# Have to wait a bit until the template becomes available
+time.sleep(20)
+
+body = open("clones.json", "r")
+conn.request("POST", "/1.3/server", body, headers)
+print(conn.getresponse().read().decode(encoding="UTF-8"))
+body.close()
 
 conn.close()
